35dc32ac740d116a1f9610afcbbbdf25752e821b
Vanya Atanasova <103636080+AtanasovaVanya@users.noreply.github.com>
2022-12-06 12:48:46 +0200

    Upload files to Data-Types-&-Variables folder.

---
+++ JavaScript-Fundamentals/Data-Types-&-Variables/biggestOfThreeNumbers.js
@@ -0,0 +1,19 @@
+function biggestOfThreeNumbers(num1, num2, num3) {
+    let biggest = 0;
+
+    if (num1 >= num2 && num1 >= num3) {
+        biggest = num1;
+    }
+    else if (num2 >= num1 && num2 >= num3) {
+        biggest = num2;
+    }
+    else {
+        biggest = num3;
+    }
+    console.log(biggest);
+
+}
+biggestOfThreeNumbers(-2,
+    7,
+    3
+);
---
+++ JavaScript-Fundamentals/Data-Types-&-Variables/binaryToDecimal.js
@@ -0,0 +1,6 @@
+function binaryToDecimal(num) {
+    let stringToNum = Number(num)
+    let decimalNum = parseInt(num, 2);
+    console.log(decimalNum);
+}
+binaryToDecimal('00001001');
