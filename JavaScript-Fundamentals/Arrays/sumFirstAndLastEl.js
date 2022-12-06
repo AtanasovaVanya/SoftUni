b442773f865e9bcf404714d19401b4ca8e9f3b08
Vanya Atanasova <103636080+AtanasovaVanya@users.noreply.github.com>
2022-12-06 13:09:24 +0200

    Create Arrays folder and upload a file.

---
+++ JavaScript-Fundamentals/Arrays/sumFirstAndLastEl.js
@@ -0,0 +1,7 @@
+function sumFirstAndLastArrayElements(arr) {
+    let firstEl = arr[0];
+    let lastEl = arr[arr.length - 1];
+    let sum = firstEl + lastEl;
+    console.log(sum);
+}
+sumFirstAndLastArrayElements([20, 30, 40]);
