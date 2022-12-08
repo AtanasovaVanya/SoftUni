1c766cc0308c394ed064f52c439bf4c0641270fe
Vanya Atanasova <103636080+AtanasovaVanya@users.noreply.github.com>
2022-12-08 10:40:48 +0200

    Add a file to Arrays folder.

---
+++ JavaScript-Fundamentals/Arrays/sumEvenNumbers.js
@@ -0,0 +1,12 @@
+function sumEvenNumbers(arr) {
+    let sum = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        let currentNum = Number(arr[i]);
+        if (currentNum % 2 === 0) {
+            sum += currentNum;
+        }
+    }
+    console.log(sum);
+}
+sumEvenNumbers(['2', '4', '6', '8', '10']);
