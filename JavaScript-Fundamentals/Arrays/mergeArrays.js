2b96d8f9a03e3a69dce6ad26217d2dafa9149426
Vanya Atanasova <103636080+AtanasovaVanya@users.noreply.github.com>
2022-12-17 11:32:24 +0200

    Add files via upload

---
+++ JavaScript-Fundamentals/Arrays/mergeArrays.js
@@ -0,0 +1,19 @@
+function mergeArrays(arr1, arr2) {
+    let arr3 = [];
+    let arrLength = arr1.length;
+
+    for (let i = 0; i < arrLength; i++) {
+        if (i % 2 === 0) {
+            arr3[i] = Number(arr1[i]) + Number(arr2[i]);
+        } else {
+            arr3[i] = arr1[i] + arr2[i];
+        }
+    }
+    console.log(arr3.join(" - "));
+}
+
+
+mergeArrays(['13', '12312', '5', '77', '4'],
+    ['22', '333', '5', '122', '44']
+
+);
