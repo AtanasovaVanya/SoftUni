5c3886b2557a8960719acefc09e5ffe4edb24ece
Vanya Atanasova <103636080+AtanasovaVanya@users.noreply.github.com>
2023-01-11 10:36:06 +0200

    Create firstAndLastKNumbers.js

---
+++ JavaScript-Fundamentals/Arrays-Advanced/firstAndLastKNumbers.js
@@ -0,0 +1,14 @@
+function firstAndLastKNumbers(arr) {
+    let kNum = arr.shift();
+    let firstArr = arr.slice(0, kNum);
+    let secondArr = arr.slice(arr.length - kNum);
+    console.log(firstArr.join(' '));
+    console.log(secondArr.join(' '));
+
+}
+firstAndLastKNumbers([2,
+    7, 8, 9]
+);
+firstAndLastKNumbers([3,
+    6, 7, 8, 9]
+);
